cdfcb0bbeff85610f9b3de55161062393588ee71
Marcel Riegler <marcel.riegler@port-neo.com>
2023-06-30 01:42:00 +0200

    add a whole bunch a plugins for fun

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,13 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.code-runner.sniprun" },
+  { import = "astrocommunity.color.twilight-nvim" },
+  { import = "astrocommunity.editing-support.zen-mode-nvim" },
+
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
